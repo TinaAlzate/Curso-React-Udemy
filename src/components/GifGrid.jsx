ebb66e87aca095317706e8966589f2c25408effa
Javier  Restrepo Toro <jarestrepot@uqvirtual.edu.co>
2023-10-04 17:25:32 +0200

    Deployment

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,24 +1,17 @@
-import { useEffect, useState } from "react";
-import { getGifs } from "../helpers/getGifs";
 import { GifItem } from "./GifItem";
+import { useFetchGifs } from "../hooks/useFetchGifs";
 
-export const GifGrid = ({ category }) => {
+export const GifGrid = ( { category } ) => {
 
-  const [images, setImages] = useState([]);
-
-  const getImages = async() => { 
-    const newImages = await getGifs(category);
-    setImages(newImages)
-  }
-
-  useEffect(() => {
-    getImages();
-  }, [ ]);
+  const {images, isLoading} = useFetchGifs(category)
   
-
   return (
     <>
       <h3>{category}</h3>
+      {
+        isLoading && <h2>Loading...</h2>
+      }
+      
       <div className="card-grid">
         {
           images.map((image) => (
